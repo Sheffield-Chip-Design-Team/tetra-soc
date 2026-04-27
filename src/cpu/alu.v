dbe633b04272543ea10abab811aaab20819e1bdb
ThomasC246 <tcowie1@sheffield.ac.uk>
2026-04-27 16:29:34 +0100

    AlU update

--- src/cpu/alu.v
+++ src/cpu/alu.v
@@ -6,40 +6,32 @@
 // =======================================================================
 
 module ALU(
-    input [3:0] A,       // First operand
-    input [3:0] B,       // Second operand
-    input submode,
-    input addmode,
-    input XORmode,
-    input NANDmode,
-    input GreaterThanmode,
-    input LessThanmode,
-    output reg [3:0] Result // ALU result
+    input [3:0] A,      
+    input [3:0] B, 
+    input [15:0] instr_in,
+    //bit 0 - 8 - Instruction bits
+    //bit 9 - 15 - Redundant.
+    
+    output reg [3:0] acc // output stored in accumulator 
+
 );
 
 always@(*) begin
-    if (submode) begin
-        Result <= A-B;
 
-    end else if (addmode) begin
-        Result <= A+B;
-
-    end else if (XORmode) begin
-        Result <= A^B;
-
-    end else if (NANDmode) begin
-        Result <= ~(A&B);
-
-    end else if (GreaterThanmode) begin
-        Result <= (A > B) ? 4'b0001 : 4'b0000; // 1 if A > B, else 0
-
-    end else if (LessThanmode) begin
-        Result <= (A < B) ? 4'b0001 : 4'b0000; // 1 if A < B, else 0
-
-    end else begin
-        Result <= 4'b0000; // Default case
-    end
-
+    case (instr_in[8:0]) // Check the instruction bits
+        11'b00100000000: acc <= A + B; // ADD
+        11'b00100000001: acc <= A - B; // SUB
+        11'b00100000010: acc <= A&B; // AND
+        11'b00100000011: acc <= !acc; // NOT - Inverting accumulator.
+        11'b00100000100: acc <= A^B ;//A XOR B
+        11'b00100000101: acc <= acc<<1; //shift left by 1
+        11'b00100000110: acc <= acc>>1; //shift right by 1
+        //11'b00100000100: GreaterThanmod = 1; // Greater Than // change the bits
+       // 11'b00100100101: LessThanmode = 1; // Less Than // changes the bits
+        default: begin
+            acc <= acc;
+        end
+    endcase
 end 
 
 endmodule
