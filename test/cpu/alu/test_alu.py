2b9a41a52358cbaf3b4ee09ef6bdcc7653c4a96b
aaiva132 <aivapersonal132@gmail.com>
2026-05-01 12:55:51 +0100

    ADD operation tested and fully functional

--- test/cpu/alu/test_alu.py
+++ test/cpu/alu/test_alu.py
@@ -1,34 +1,5 @@
 import cocotb
 from random import randint
-<<<<<<< HEAD
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, RisingEdge
-import os
-
-async def reset(uut, reset_duration=randint(1,10)):
-    # assert reset
-    uut._log.info("Resetting Module")
-    uut.rst.value = 1
-    await ClockCycles(uut.clk, reset_duration)
-    uut.rst.value = 0
-
-@cocotb.test()
-async def test_alu_sanity(uut):
-    # start clock
-    clock = Clock(uut.clk, 40, units="ns")
-    cocotb.start_soon(clock.start())
-    await ClockCycles(uut.clk, 1)
-
-    # reset the module
-    await reset(uut)
-    await RisingEdge(uut.clk)
-
-    # continue test ...
-    await ClockCycles(uut.clk, 100)
-    uut._log.info("Test Complete!")
-
-    
-=======
 from cocotb.triggers import Timer
 
 @cocotb.test()
@@ -51,49 +22,48 @@ async def test_alu_combinational(uut):
   uut._log.info("Initial Values Set")
   uut._log.info("***************************************************")
   uut._log.info("Testing ADD operation:")
-  uut._log.info("No Overflow:")
   
-  uut.reg_a_val.value = 5
-  uut.reg_b_val.value = 7
+  op_pass = False
+  test_pass = True
+
   uut.alu_control.value = 1
 
+  for i in range(0,16):
+    for j in range(0,16):
+      op_pass = True
+
+      uut.reg_a_val.value = i
+      uut.reg_b_val.value = j
+
+      await Timer(5, unit="ns")
+
+      reg_a = int(uut.reg_a_val.value)
+      reg_b = int(uut.reg_b_val.value)
+      result = int(uut.result.value)
+      overflow = int(uut.overflow.value)
+
+      if (
+        (not i + j > 15 and 
+        overflow == 1 and
+        i + j - 16 == result)
+        and
+        (not i + j == result and
+         overflow == 0)
+        ):
+        op_pass = False
+        test_pass = False
+      
+      uut._log.info(
+        f"Reg A: {reg_a}\tReg B: {reg_b}\tResult: {result}\tOverflow: {overflow}\tOperation Result: {op_pass}"
+        )
+      
+  uut.alu_control.value = 0
+
   await Timer(5, unit="ns")
-
-  op_pass = False
-  reg_a = int(uut.reg_a_val.value)
-  reg_b = int(uut.reg_b_val.value)
-  result = int(uut.result.value)
-  overflow = int(uut.overflow.value)
-
-
-  if (result == reg_a + reg_b) and (overflow == 0):
-    op_pass = True
-  
-  uut._log.info(
-    f"Reg A: {reg_a}, Reg B: {reg_b}, Result: {result}, Overflow: {overflow}, Test Result: {op_pass}"
-    )
-  
-  uut._log.info("With Overflow")
-
-  uut.reg_a_val.value = 12
-  uut.reg_b_val.value = 7
-
-  await Timer(5, unit="ns")
-
-  op_pass = False
-  reg_a = int(uut.reg_a_val.value)
-  reg_b = int(uut.reg_b_val.value)
-  result = int(uut.result.value)
-  overflow = int(uut.overflow.value)
-
-  if (result == (reg_a + reg_b) - 16) and (overflow == 1):
-    op_pass = True
-
-  uut._log.info(
-    f"Reg A: {reg_a}, Reg B: {reg_b}, Result: {result}, Overflow: {overflow}, Test Result: {op_pass}"
-    )
+      
+  uut._log.info("***************************************************")
+  uut._log.info(f"ADD testing finished.\tPassed:\t{test_pass}")
 
   # TODO - add checks for expected output values
 
   uut._log.info("Test Complete!")
->>>>>>> dev
