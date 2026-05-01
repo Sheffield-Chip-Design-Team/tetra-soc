e872778efcb194f6cfa43cf54d849067a0a3413d
aaiva132 <aivapersonal132@gmail.com>
2026-05-01 03:08:42 +0100

    Fixed makefile

--- test/cpu/alu/alu_wtb.v
+++ test/cpu/alu/alu_wtb.v
@@ -6,22 +6,6 @@
 module alu_wtb;
 
   // alu instantation signals
-<<<<<<< HEAD
-  reg   clk;
-  reg   rst;
-  reg  [3:0] A;
-  reg  [3:0] B;
-  reg  [15:0] instr_in;
-  wire [3:0] acc;
-
-alu dut (
-      .clk(clk),
-      .rst(rst),
-      .A(A),
-      .B(B),
-      .instr_in(instr_in),
-      .acc(acc)
-=======
   reg  [3:0] reg_a_val;
   reg  [3:0] reg_b_val;
   reg  [3:0] acc;
@@ -36,7 +20,6 @@ alu dut (
       .alu_control(alu_control),
       .result(result),
       .overflow(overflow)
->>>>>>> dev
   );
 
 endmodule 
