4f35548194e9058692790d01a457e39600e96d83
ThomasC246 <tcowie1@sheffield.ac.uk>
2026-05-05 23:49:52 +0100

    CU 05/05/2026 changes

--- src/cpu/alu.v
+++ src/cpu/alu.v
@@ -5,33 +5,43 @@
 //              and logic operations based on control signals.
 // =======================================================================
 
-module ALU(
+module alu(
+    input clk,
+    input rst, // debug only
     input [3:0] A,      
     input [3:0] B, 
     input [15:0] instr_in,
     //bit 0 - 8 - Instruction bits
     //bit 9 - 15 - Redundant.
     
+    
     output reg [3:0] acc // output stored in accumulator 
 
 );
 
-always@(*) begin
+always@(posedge clk ) begin
 
-    case (instr_in[8:0]) // Check the instruction bits
-        11'b00100000000: acc <= A + B; // ADD
-        11'b00100000001: acc <= A - B; // SUB
-        11'b00100000010: acc <= A&B; // AND
-        11'b00100000011: acc <= !acc; // NOT - Inverting accumulator.
-        11'b00100000100: acc <= A^B ;//A XOR B
-        11'b00100000101: acc <= acc<<1; //shift left by 1
-        11'b00100000110: acc <= acc>>1; //shift right by 1
-        //11'b00100000100: GreaterThanmod = 1; // Greater Than // change the bits
-       // 11'b00100100101: LessThanmode = 1; // Less Than // changes the bits
-        default: begin
-            acc <= acc;
-        end
-    endcase
+    if (rst) begin
+        acc <= 4'b0000; // Reset accumulator to 0 on reset
+    end else begin
+        
+        case (instr_in[8:0]) // Check the instruction bits
+            11'b00100000000: acc <= A + B; // ADD
+            11'b00100000001: acc <= A - B; // SUB
+            11'b00100000010: acc <= A&B; // AND
+            11'b00100000011: acc <= !acc; // NOT - Inverting accumulator.
+            11'b00100000100: acc <= A^B ;//A XOR B
+            11'b00100000101: acc <= acc<<1; //shift left by 1
+            11'b00100000110: acc <= acc>>1; //shift right by 1
+            //11'b00100000100: GreaterThanmod = 1; // Greater Than // change the bits
+           // 11'b00100100101: LessThanmode = 1; // Less Than // changes the bits
+            default: begin
+                acc <= acc;
+            end
+        endcase
+
+    end
+
 end 
 
 endmodule
