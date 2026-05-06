75850799ee10ea12f9ab30528c30874d464a8f3f
aaiva132 <aivapersonal132@gmail.com>
2026-05-06 18:14:24 +0100

    Implemented register file

--- src/cpu/reg_file.v
+++ src/cpu/reg_file.v
@@ -5,19 +5,40 @@
 // =======================================================================
 
 module reg_file #(
-  parameter DATA_WIDTH = 4     
+  parameter DATA_WIDTH = 4
 )(
-  input                     clk,
-  input                     rst_n,
-  input  [1:0]              reg_wstrb,
-  input  [DATA_WIDTH-1:0]  ra_d,
-  input  [DATA_WIDTH-1:0]  rb_in,
-  output [DATA_WIDTH-1:0]  ra_out,
-  output [DATA_WIDTH-1:0]  rb_out, 
+  input             clk,
+  input             a_en,
+  input             b_en,
+  input             s_en,
+  input             of_in,
+  input             uf_in,
+  input       [3:0] d_in,
+  output  reg [3:0] ra_out,
+  output  reg [3:0] rb_out, 
+  output  reg [3:0] rs_out
 );
 
- // TODO write the register file logic
+wire  [3:0] a_mask;
+wire  [3:0] b_mask;
+wire  [3:0] s_mask;
 
+wire  [3:0] s_val;
+
+assign  a_mask = d_in & {4{a_en}};
+assign  b_mask = d_in & {4{b_en}};
+assign  s_val = rs_out;
+
+assign  s_mask[0] = (d_in[0] & {s_en}) | of_in | (rs_out[0] & !s_en);
+assign  s_mask[1] = (d_in[1] & {s_en}) | uf_in | (rs_out[1] & !s_en);
+assign  s_mask[3:2] = (d_in[3:2] & {2{s_en}}) | (rs_out[3:2] & {2{!s_en}});
+
+ // TODO write the register file logic
+always @(posedge clk) begin
+  ra_out <= a_mask;
+  rb_out <= b_mask;
+  rs_out <= s_mask;
+end
 
 endmodule
 
