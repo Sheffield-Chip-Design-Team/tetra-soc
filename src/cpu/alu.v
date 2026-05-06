6b83dcfa417ba3242234b6647b9a7a918924d2a6
aaiva132 <aivapersonal132@gmail.com>
2026-05-06 03:12:57 +0100

    Annotated alu.v

--- src/cpu/alu.v
+++ src/cpu/alu.v
@@ -32,22 +32,27 @@ assign use_uf = uf_in & alu_control[4];
 always @* begin
     if (alu_control[5]) begin
         case (alu_control[3:0])
-            4'b0000: {of_set, result} <= acc_val + 1 + use_of;
-            4'b0001: {uf_set, result} <= acc_val - 1 - use_uf;
-            4'b0010: {of_set, result} <= {acc_val, use_of} << 1;
-            4'b0011: {result, uf_set} <= {use_uf, acc_val} >> 1;
-            4'b0100,
-            4'b0110: {of_set, result} <= inp_a + inp_b + use_of;
-            4'b0101,
+            4'b0000: {of_set, result} <= acc_val + 1 + use_of;      //  INC / INCC
+            4'b0001: {uf_set, result} <= acc_val - 1 - use_uf;      //  DEC / DECB
+            4'b0010: {of_set, result} <= {acc_val, use_of} << 1;    //  LSH / LSHC
+            4'b0011: {result, uf_set} <= {use_uf, acc_val} >> 1;    //  RSH / RSHB
+
+            4'b0100,                                                // ADD / ADDC
+            4'b0110: {of_set, result} <= inp_a + inp_b + use_of;    //
+
+            4'b0101,                                                // SUB / SUBB
             4'b0111: {uf_set, result} <= inp_a - inp_b - use_of;
-            4'b1000,
-            4'b1100: result <= inp_a & inp_b;
-            4'b1001,
-            4'b1101: result <= inp_a | inp_b;
-            4'b1010,
-            4'b1011,
-            4'b1110,
-            4'b1111: result <= inp_a ^ inp_b;
+
+            4'b1000,                            //  AND
+            4'b1100: result <= inp_a & inp_b;   //
+
+            4'b1001,                            // OR
+            4'b1101: result <= inp_a | inp_b;   //
+
+            4'b1010,                            // XOR
+            4'b1011,                            //
+            4'b1110,                            //
+            4'b1111: result <= inp_a ^ inp_b;   //
         endcase
     end
 end
