4c56168ed3fc79c504e28dd7f343da841f6b9386
James <162901067+Enjimneering@users.noreply.github.com>
2026-05-07 20:25:58 +0100

    delete graphics hardware

--- src/irq/irq.v
+++
@@ -1,5 +0,0 @@
-// Interrupt contoller that schedules interrupts based on priority and pending status. 
-// It also handles the interrupt acknowledge cycle and provides the interrupt vector to the CPU.
-
-
-
