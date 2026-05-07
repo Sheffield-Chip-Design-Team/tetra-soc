a6af7c38d5e77018c6fe156a42c2e93dcfcc0dd6
James <162901067+Enjimneering@users.noreply.github.com>
2026-05-07 20:26:30 +0100

    restructure interconnect tb

--- test/intercon/intercon_tb.v
+++ test/intercon/intercon_tb.v
@@ -2,13 +2,6 @@
 `timescale 1ns/1ps
 
 module intercon_tb;
-  // Dump waves
-  initial begin
-    $dumpfile("intercon_tb.fst");
-    $dumpvars(0, intercon_tb);
-    #1;
-  end
-
   // Clock / reset
   reg sys_clk;
   reg sys_rst_n;
@@ -288,56 +281,10 @@ module intercon_tb;
     .wb_ram_rty_i         (wb_ram_rty_i)
   );
 
-endmodule
-
-// -----------------------------------------------------------------------------
-// Full Wishbone (8-bit data) peripheral wrapper for wb_spi_mem_ctrl
-// -----------------------------------------------------------------------------
-module wb_spi_mem_ctrl_wb (
-  input  wire        clk,
-  input  wire        rst_n,
-
-  input  wire [31:0] wb_adr_i,
-  input  wire  [7:0] wb_dat_i,
-  input  wire  [3:0] wb_sel_i,
-  input  wire        wb_we_i,
-  input  wire        wb_cyc_i,
-  input  wire        wb_stb_i,
-  input  wire  [2:0] wb_cti_i,
-  input  wire  [1:0] wb_bte_i,
-
-  output wire  [7:0] wb_rdt_o,
-  output wire        wb_ack_o,
-  output wire        wb_err_o,
-  output wire        wb_rty_o,
-
-  output wire        cs_n,
-  output wire        sck,
-  output wire        mosi,
-  input  wire        miso
-);
-  // Unused in this simple adapter
-  wire _unused = &{1'b0, wb_sel_i, wb_cti_i, wb_bte_i, wb_adr_i[31:5]};
-
-  wb_spi_mem_ctrl u (
-    .clk      (clk),
-    .rst_n    (rst_n),
-
-    .wb_addr_i (wb_adr_i[4:0]),
-    .wb_wdata_i(wb_dat_i),
-    .wb_we_i   (wb_we_i),
-    .wb_stb_i  (wb_cyc_i & wb_stb_i),
-    .wb_rdata_o(wb_rdt_o),
-    .wb_ack_o  (wb_ack_o),
-    .irq_o     (),
-
-    .cs_n      (cs_n),
-    .sck       (sck),
-    .mosi      (mosi),
-    .miso      (miso)
-  );
-
-  assign wb_err_o = 1'b0;
-  assign wb_rty_o = 1'b0;
+  initial begin
+    $dumpfile("intercon_tb.fst");
+    $dumpvars(0, intercon_tb);
+    #1;
+  end
 
 endmodule
